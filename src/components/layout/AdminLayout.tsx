4d20fd46e01d3f95249e6dd61ea91b57c9e905fd
neukkken <santyagonarvaezns@gmail.com>
2025-04-14 15:27:19 -0500

    fix: change logo directory from public to assets

--- src/components/layout/AdminLayout.tsx
+++ src/components/layout/AdminLayout.tsx
@@ -8,6 +8,7 @@ import {
 } from "lucide-react"
 import SideBar from "../ui/SideBar"
 import { Outlet } from "react-router-dom"
+import logo from "../../assets/favicon-512x512.png";
 
 const sideBarProps = [
     { title: "Contratistas", icon: <HandshakeIcon />, path: "/admin/contratistas" },
@@ -28,7 +29,7 @@ const AdminLayout = () => {
 
             {/* ✅ Header */}
             <header className="h-20 px-6 flex items-center gap-4 border-b border-white/10 bg-[#060911] shadow-[0_2px_10px_rgba(0,0,0,0.3)] relative">
-                <img src="/favicon-512x512.png" alt="Logo" className="w-14 h-14" />
+                <img src={logo} alt="Logo" className="w-14 h-14" />
                 <h1 className="text-[1em] font-semibold text-white tracking-wide">Administrador CTL</h1>
             </header>
 
